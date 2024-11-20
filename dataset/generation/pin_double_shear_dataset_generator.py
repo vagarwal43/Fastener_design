1b3f66efe63dc0f150fb3604f99d850dd74d7e55
Daniele Grandi <daniele.grandi@autodesk.com>
2024-11-20 15:12:10 -0800

    minor fix, test export working

--- dataset/generation/pin_double_shear_dataset_generator.py
+++ dataset/generation/pin_double_shear_dataset_generator.py
@@ -3,18 +3,17 @@ from math import pi, sqrt
 
 # function to determing the mininum viable pin diameter for a pin in double shear giventhe following, and assuming a nominal area:
 # force, factor of safety, yield strength
-
 def min_pin_diameter(F, FoS, Sy):
     d = sqrt((2 * F * FoS) / (pi * 0.5 * Sy))
     return d
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     # create the dataset based on a set of forces, safety factors, and yield strengths
     data = []
-    for F in []:
-        for FoS in []:
-            for Sy in []:
+    for F in [1]:
+        for FoS in [1]:
+            for Sy in [1]:
                 d = min_pin_diameter(F, FoS, Sy)
 
                 # TODO: take the ceiling of d or base on available library
@@ -22,6 +21,7 @@ if __name__ == 'main':
                 data.append([F, FoS, Sy, d])
 
     # save the data to a csv file
-    with open('pin_double_shear_dataset.csv', 'w') as f:
+    with open('../pin_double_shear_dataset.csv', 'w') as f:
+        f.write('F,FoS,Sy,d\n')
         for row in data:
             f.write(','.join(map(str, row)) + '\n')
