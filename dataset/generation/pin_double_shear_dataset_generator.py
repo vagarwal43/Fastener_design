e213b13404a0e32e8ae64608532e79feb8cbcb75
Daniele Grandi <daniele.grandi@autodesk.com>
2024-11-21 10:32:17 -0800

    minor cleanup

--- dataset/generation/pin_double_shear_dataset_generator.py
+++ dataset/generation/pin_double_shear_dataset_generator.py
@@ -1,13 +1,15 @@
 from math import pi, sqrt
 
 
-# function to determining the minimum viable pin diameter for a pin in double shear given the following, and assuming a
-# nominal area, and that allowable shear ~= 0.5*yield strength: force, factor of safety, yield strength
-# F: force in N
-# FoS: factor of safety
-# Sy: yield strength in MPa
-# returns: minimum pin diameter in mm
 def min_pin_diameter(F, FoS, Sy):
+    '''
+    function to determining the minimum viable pin diameter for a pin in double shear given the following, and assuming
+    a nominal area, and that allowable shear ~= 0.5*yield strength: force, factor of safety, yield strength.
+    F: force in N
+    FoS: factor of safety
+    Sy: yield strength in MPa
+    returns: minimum pin diameter in mm
+    '''
     d = sqrt((2 * F * FoS) / (pi * 0.5 * Sy))
     return d
 
@@ -30,7 +32,7 @@ def pin_diameter_tool(F, FoS, Sy, available_diameters):
 if __name__ == '__main__':
     # test min_pin_diameter function with force = 100N, FoS = 2, Sy = 250MPa
     print("Test min_pin_diameter function with force = 100N, FoS = 2, Sy = 250MPa")
-    print(f"min pin diameter: {min_pin_diameter(100, 2, 250)} mm")
+    print(f"min pin diameter: {min_pin_diameter(100, 2, 250)} mm = 1.01 mm")
 
     available_diameters = [0.5, 1, 1.5, 2, 2.5, 3, 4, 5, 6, 8, 10, 12, 16, 20, 25, 30, 40, 50]  # mm
     forces = [1]  # N
